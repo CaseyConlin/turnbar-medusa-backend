2ec45fb115617226a25d27bc640e24e06537057e
Casey Conlin <101153261+CaseyConlin@users.noreply.github.com>
2024-04-17 11:28:18 -0400

    Deploy DO.

--- medusa-config.js
+++ medusa-config.js
@@ -93,7 +93,17 @@ module.exports = {
   projectConfig: {
     // ...
     // database_extra: { ssl: { rejectUnauthorized: false } },
+
     redis_url: process.env.REDIS_URL,
+    jwtSecret: process.env.JWT_SECRET,
+    cookieSecret: process.env.COOKIE_SECRET,
+    store_cors: STORE_CORS,
+    database_url: DATABASE_URL,
+    // database_extra: { ssl: { rejectUnauthorized: false } },
+    admin_cors: ADMIN_CORS,
+    // Uncomment the following lines to enable REDIS
+    redis_url: REDIS_URL,
+
     database_extra:
       process.env.NODE_ENV !== "development"
         ? { ssl: { rejectUnauthorized: false } }
